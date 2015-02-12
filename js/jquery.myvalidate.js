e620fdd9b54f630a9e59747c3e0e95c60ba40ccd
zhangxiaotianmacpor <zhangxiaotian@zhangxiaotian.local>
2015-02-12 22:56:48 +0800

    sfw

--- js/jquery.myvalidate.js
+++ js/jquery.myvalidate.js
@@ -1,53 +1,63 @@
-$("form").find("input[type='text']").bind({
-	'blur': function() {
-		$this = $(this);
-		var $value = $this.val()
-		var datarule = $this.attr("data-check");
-		var errorspan = $this.parent("div").parent("td").next("td").find("span");
-		if (datarule != undefined) {
-			var dataarray = datarule.split(",")
-			v_fn()
-		}
+function form_validate(form,errortype) {
+	var errorspan2 = "";
+	$(form).find("input[type='text']").bind({
 
-		function data_inarray(rule_name) {
-			return $.inArray(rule_name, dataarray)
-		}
+		'blur': function() {
+			$this = $(this);
+			var $value = $this.val()
+			var datarule = $this.attr("data-check");
+			var errorspan1 = $this.parent("div").parent("td").next("td").find("span");
+			
+			if (datarule != undefined) {
+				var dataarray = datarule.split(",")
+				v_fn()
+			}
 
-		function v_fn() {
-			if (data_inarray("required") != -1) {
-				if ($.trim($value) == "") {
-					error_add("此项为必填")
+			function data_inarray(rule_name) {
+				return $.inArray(rule_name, dataarray)
+			}
+
+			function v_fn() {
+				if (data_inarray("required") != -1) {
+					if ($.trim($value) == "") {
+						error_add("此项为必填")
+					}
+				}
+				if (data_inarray("username") != -1) {
+					var reg = /^[\u4e00-\u9fa5]+$/i;
+					if ($.trim($value) == "") {
+						error_add("请输入您的姓名")
+					} else if (!reg.test($value)) {
+						error_add("请输入正确的姓名")
+					}
+				}
+				if (data_inarray("tel") != -1) {
+					var isMobile = /^1[3|4|5|7|8]\d{9}$/;
+					var isPhone = /^((0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/;
+					if (!isMobile.test($value) && !isPhone.test($value)) {
+						error_add("请输入正确的电话号码")
+					}
 				}
 			}
-			if (data_inarray("username") != -1) {
-				var reg = /^[\u4e00-\u9fa5]+$/i;
-				if ($.trim($value) == "") {
-					error_add("请输入您的姓名")
-				} else if (!reg.test($value)) {
-					error_add("请输入正确的姓名")
+
+			//添加错误样式
+
+			function error_add(text) {
+				if(errortype==1){
+				errorspan1.html(text).addClass("error_yellow")
+				}else if( errortype==2){
+				errorspan2.html(text)	
 				}
 			}
-			if (data_inarray("tel") != -1) {
-				var isMobile = /^1[3|4|5|7|8]\d{9}$/;
-				var isPhone = /^((0\d{2,3})-)?(\d{7,8})(-(\d{3,}))?$/;
-				if(!isMobile.test($value) && !isPhone.test($value)){
-					error_add("请输入正确的电话号码")
-				}
+
+		},
+		'focus': function() {
+			$this = $(this)
+			var errorspan1 = $this.parent("div").parent("td").next("td").find("span");
+			if (errorspan1.hasClass("error_yellow")) {
+				errorspan1.removeClass("error_yellow").html("")
 			}
 		}
-
-		//添加错误样式
-
-		function error_add(text) {
-			errorspan.html(text).addClass("error_yellow")
-		}
-
-	},
-	'focus': function() {
-		$this = $(this)
-		var errorspan = $this.parent("div").parent("td").next("td").find("span");
-		if (errorspan.hasClass("error_yellow")) {
-			errorspan.removeClass("error_yellow").html("")
-		}
-	}
-})
+	})
+}
+form_validate("#register1",1)
