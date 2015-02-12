3086105bbd36fc03fac7bfa4c816d67dd02f795f
zhangxiaotianmacpor <zhangxiaotian@zhangxiaotian.local>
2015-02-12 17:59:35 +0800

    sss

--- js/scroll_top.js
+++ js/scroll_top.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 	var scroll_div= '<div class="back_top"><ul><li id="bk-top">返回顶部^</li></ul></div>'
     $("html body").append(scroll_div)
 })
+var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
 
 $(window).scroll(function() {
 	if ($(window).scrollTop() > 100) {
