9cd2dc05f1ab50cd25235cd2c2ec955d1c83509d
zhangxiaotianmacpor <zhangxiaotian@zhangxiaotian.local>
2015-02-12 21:38:21 +0800

    fwefwe

--- js/scroll_top.js
+++ js/scroll_top.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-	var scroll_div= '<div class="back_top"><ul><li id="bk-top"><a href="#top" style="color:#888">返回顶部^</a></li></ul></div>'
-    $("html body").append(scroll_div)
-})
+//$(document).ready(function() {
+//	var scroll_div= '<div class="back_top"><ul><li id="bk-top"><a href="#top" style="color:#888">返回顶部^</a></li></ul></div>'
+//  $("html body").append(scroll_div)
+//})
 var scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
 
 $(window).scroll(function() {
